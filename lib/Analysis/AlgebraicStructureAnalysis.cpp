ab85a47825eb35cd02b14c6d4c6aaa9e944d8232
Danila Seliayeu <seliayeu@ualberta.ca>
2026-02-09 16:59:28 -0700

    chore: refactor analysis code

--- lib/Analysis/AlgebraicStructureAnalysis.cpp
+++ lib/Analysis/AlgebraicStructureAnalysis.cpp
@@ -25,6 +25,32 @@ LogicalResult AlgebraicStructureAnalysis::run(Block* block) {
     return success();
 }
 
+SubMatrixProperty AlgebraicStructureAnalysis::readPropertyFromDictAttr(DictionaryAttr dictAttr) {
+    auto metadataAttr{ dictAttr.get("metadata") };
+    if (!metadataAttr)
+        return { AlgebraicProperty::General };
+    auto innerDictAttr{ dyn_cast<DictionaryAttr>(metadataAttr) };
+    if (!innerDictAttr)
+        return { AlgebraicProperty::General };
+    auto analysisPropertyAttr{ innerDictAttr.get("analysisState") };
+    if (!analysisPropertyAttr)
+        return { AlgebraicProperty::General };
+    auto analysisProperty{ dyn_cast<StringAttr>(analysisPropertyAttr).getValue() };
+
+    auto propertyDimsAttr{ innerDictAttr.get("propertyDims") };
+    if (!propertyDimsAttr)
+        return { AlgebraicProperty::General };
+    auto propertyDimsArrayAttr{ dyn_cast<ArrayAttr>(propertyDimsAttr) };
+    if (!propertyDimsArrayAttr || propertyDimsArrayAttr.size() != 2) {
+        return { AlgebraicProperty::General };
+    }
+    SubMatrixProperty res{ stringToValue(std::string(analysisProperty)) }; 
+    res.dimensions[0] = cast<IntegerAttr>(propertyDimsArrayAttr[0]).getInt();
+    res.dimensions[1] = cast<IntegerAttr>(propertyDimsArrayAttr[1]).getInt();
+
+    return res;
+}
+
 LogicalResult AlgebraicStructureAnalysis::visitOperation(Operation *op) {
     if (op->getNumResults() != 1)
         return success();
@@ -43,103 +69,139 @@ LogicalResult AlgebraicStructureAnalysis::visitOperation(Operation *op) {
     propertyMap[result] = readPropertyFromDictAttr(dict);
 
     if (auto matmulOp{ dyn_cast<linalg::MatmulOp>(op) }) {
-        auto operands{ matmulOp->getOperands() };
-        auto lhs{ operands[0] };
-        auto rhs{ operands[1] };
-        auto lhsType{ dyn_cast<TensorType>(lhs.getType()) };
-        auto rhsType{ dyn_cast<TensorType>(rhs.getType()) };
-        auto lhsShape{ lhsType.getShape() };
-        auto rhsShape{ rhsType.getShape() };
-        if (!(lhsShape[0] == lhsShape[1]) || !(rhsShape[0] == rhsShape[1])) 
-            return success();
-
-        auto indexingMapArr{ matmulOp.getIndexingMapsArray() };
-        if (indexingMapArr.size() != 3)
-            return success();
-
-        auto* ctx{ matmulOp->getContext() };
-        AffineExpr m, n, k;
-        bindDims(ctx, m, n, k);
-        auto expectedMapLhs{ AffineMap::get(3, 0, {m, k}, ctx) };
-        auto expectedMapRhs{ AffineMap::get(3, 0, {k, n}, ctx) };
-        auto expectedMapRes{ AffineMap::get(3, 0, {m, n}, ctx) };
-
-        if (indexingMapArr[0] != expectedMapLhs || 
-            indexingMapArr[1] != expectedMapRhs || 
-            indexingMapArr[2] != expectedMapRes) {
-            return success();
-        }
-
-        auto newProperty{ binaryMatmul(propertyMap[lhs].property, propertyMap[rhs].property) };
-        newProperty = join(propertyMap[result].property, newProperty);
-        propertyMap[result] = SubMatrixProperty{ newProperty, { 0, 1 } };
+        return visitMatmul(&matmulOp);
     } else if (auto addOp{ dyn_cast<linalg::AddOp>(op) }) {
-        auto operands{ addOp.getOperands() };
-        auto lhs{ operands[0] };
-        auto rhs{ operands[1] };
-        auto lhsType{ dyn_cast<TensorType>(lhs.getType()) };
-        auto rhsType{ dyn_cast<TensorType>(rhs.getType()) };
-        auto lhsShape{ lhsType.getShape() };
-        auto rhsShape{ rhsType.getShape() };
-        if (!(lhsShape[0] == lhsShape[1]) || !(rhsShape[0] == rhsShape[1])) 
-            return success();
-        
-        if (propertyMap[lhs].dimensions != propertyMap[rhs].dimensions)
-            return success();
-        AlgebraicProperty newProperty;
-        newProperty = binaryElementwiseGeneral(propertyMap[lhs].property, propertyMap[rhs].property);
-        propertyMap[result] = {
-            join(propertyMap[result].property, newProperty),
-            propertyMap[operands[0]].dimensions
-        };
+        return visitAdd(&addOp);
     } else if (auto elementwiseOp{ dyn_cast<linalg::ElementwiseOp>(op)}) {
-        for (auto& map : elementwiseOp.getIndexingMapsArray())
-            if (!map.isIdentity()) 
-                return success();
-        auto operands{ elementwiseOp.getOperands() };
-        if (operands.size() == 1) {
-            auto newProperty{ unaryElementwise(propertyMap[operands[0]].property) };
-            propertyMap[result].property = join(propertyMap[result].property, newProperty);
-        } else if (operands.size() == 2) {
-            if (propertyMap[operands[0]].dimensions != propertyMap[operands[1]].dimensions)
-                return success();
-            auto newProperty{ binaryElementwiseGeneral(propertyMap[operands[0]].property,
-                propertyMap[operands[1]].property) };
-            newProperty = join(propertyMap[result].property, newProperty);
-            propertyMap[result] = {
-                join(propertyMap[result].property, newProperty),
-                propertyMap[operands[0]].dimensions
-            };
-        }
+        return visitElementwise(&elementwiseOp);
     } else if (auto mulOp{ dyn_cast<linalg::MulOp>(op)}) {
-        for (auto& map : mulOp.getIndexingMapsArray())
-            if (!map.isIdentity()) 
-                return success();
-        auto operands{ mulOp.getOperands() };
-        if (propertyMap[operands[0]].dimensions != propertyMap[operands[1]].dimensions)
-            return success();
-        auto newProperty{ binaryElementwiseProduct(propertyMap[operands[0]].property,
-            propertyMap[operands[1]].property)};
-        propertyMap[result] = {
-            join(propertyMap[result].property, newProperty),
-            propertyMap[operands[0]].dimensions
-        };
+        return visitMul(&mulOp);
     } else if (auto transposeOp{ dyn_cast<linalg::TransposeOp>(op)}) {
-        auto operands{ transposeOp.getOperands() };
-        auto permutation{ transposeOp.getPermutation() };
-        if (permutation.size() != 2)
-            return success();
-        if (permutation[0] != propertyMap[operands[0]].dimensions[1]
-                && permutation[1] != propertyMap[operands[0]].dimensions[0])
-            return success();
-
-        auto newProperty{ transpose(propertyMap[operands[0]].property) };
-        propertyMap[result] = {
-            join(propertyMap[result].property, newProperty),
-            { propertyMap[operands[0]].dimensions[1], propertyMap[operands[0]].dimensions[0] }
-        };
+        return visitTranspose(&transposeOp);
     } 
 
+    return success();
+}
+
+
+LogicalResult AlgebraicStructureAnalysis::visitMatmul(linalg::MatmulOp* matmulOp) {
+    auto operands{ matmulOp->getOperands() };
+    auto result{ matmulOp->getResult(0) };
+    auto lhs{ operands[0] };
+    auto rhs{ operands[1] };
+    auto lhsType{ dyn_cast<TensorType>(lhs.getType()) };
+    auto rhsType{ dyn_cast<TensorType>(rhs.getType()) };
+    auto lhsShape{ lhsType.getShape() };
+    auto rhsShape{ rhsType.getShape() };
+
+    if (!(lhsShape[0] == lhsShape[1]) || !(rhsShape[0] == rhsShape[1])) 
+        return success();
+
+    auto indexingMapArr{ matmulOp->getIndexingMapsArray() };
+    if (indexingMapArr.size() != 3)
+        return success();
+
+    auto* ctx{ matmulOp->getContext() };
+    AffineExpr m, n, k;
+    bindDims(ctx, m, n, k);
+    auto expectedMapLhs{ AffineMap::get(3, 0, {m, k}, ctx) };
+    auto expectedMapRhs{ AffineMap::get(3, 0, {k, n}, ctx) };
+    auto expectedMapRes{ AffineMap::get(3, 0, {m, n}, ctx) };
+
+    if (indexingMapArr[0] != expectedMapLhs || 
+        indexingMapArr[1] != expectedMapRhs || 
+        indexingMapArr[2] != expectedMapRes) {
+        return success();
+    }
+
+    auto newProperty{ binaryMatmul(propertyMap[lhs].property, propertyMap[rhs].property) };
+    newProperty = join(propertyMap[result].property, newProperty);
+    propertyMap[result] = SubMatrixProperty{ newProperty, { 0, 1 } };
+
+    return success();
+}
+
+LogicalResult AlgebraicStructureAnalysis::visitAdd(linalg::AddOp* addOp) {
+    auto operands{ addOp->getOperands() };
+    auto result{ addOp->getResult(0) };
+    auto lhs{ operands[0] };
+    auto rhs{ operands[1] };
+    auto lhsType{ dyn_cast<TensorType>(lhs.getType()) };
+    auto rhsType{ dyn_cast<TensorType>(rhs.getType()) };
+    auto lhsShape{ lhsType.getShape() };
+    auto rhsShape{ rhsType.getShape() };
+    if (!(lhsShape[0] == lhsShape[1]) || !(rhsShape[0] == rhsShape[1])) 
+        return success();
+    
+    if (propertyMap[lhs].dimensions != propertyMap[rhs].dimensions)
+        return success();
+    AlgebraicProperty newProperty;
+    newProperty = binaryElementwiseGeneral(propertyMap[lhs].property, propertyMap[rhs].property);
+    propertyMap[result] = {
+        join(propertyMap[result].property, newProperty),
+        propertyMap[operands[0]].dimensions
+    };
+    return success();
+}
+
+LogicalResult AlgebraicStructureAnalysis::visitMul(linalg::MulOp* mulOp) {
+    for (auto& map : mulOp->getIndexingMapsArray())
+        if (!map.isIdentity()) 
+            return success();
+    auto operands{ mulOp->getOperands() };
+    auto result{ mulOp->getResult(0) };
+    if (propertyMap[operands[0]].dimensions != propertyMap[operands[1]].dimensions)
+        return success();
+    auto newProperty{ binaryElementwiseProduct(propertyMap[operands[0]].property,
+        propertyMap[operands[1]].property)};
+    propertyMap[result] = {
+        join(propertyMap[result].property, newProperty),
+        propertyMap[operands[0]].dimensions
+    };
+    return success();
+}
+
+LogicalResult AlgebraicStructureAnalysis::visitTranspose(linalg::TransposeOp* transposeOp) {
+    auto operands{ transposeOp->getOperands() };
+    auto resultRange{ transposeOp->getResult() };
+    auto result{ resultRange[0] };
+    auto permutation{ transposeOp->getPermutation() };
+    if (permutation.size() != 2)
+        return success();
+    if (permutation[0] != propertyMap[operands[0]].dimensions[1]
+            && permutation[1] != propertyMap[operands[0]].dimensions[0])
+        return success();
+
+    auto newProperty{ transpose(propertyMap[operands[0]].property) };
+    propertyMap[result] = {
+        join(propertyMap[result].property, newProperty),
+        { propertyMap[operands[0]].dimensions[1], propertyMap[operands[0]].dimensions[0] }
+    };
+    return success();
+}
+
+LogicalResult AlgebraicStructureAnalysis::visitElementwise(linalg::ElementwiseOp* elementwiseOp) {
+    for (auto& map : elementwiseOp->getIndexingMapsArray())
+        if (!map.isIdentity()) 
+            return success();
+
+    auto operands{ elementwiseOp->getOperands() };
+    auto result{ elementwiseOp->getResult(0) };
+
+    if (operands.size() == 1) {
+        auto newProperty{ unaryElementwise(propertyMap[operands[0]].property) };
+        propertyMap[result].property = join(propertyMap[result].property, newProperty);
+    } else if (operands.size() == 2) {
+        if (propertyMap[operands[0]].dimensions != propertyMap[operands[1]].dimensions)
+            return success();
+        auto newProperty{ binaryElementwiseGeneral(propertyMap[operands[0]].property,
+            propertyMap[operands[1]].property) };
+        newProperty = join(propertyMap[result].property, newProperty);
+        propertyMap[result] = {
+            join(propertyMap[result].property, newProperty),
+            propertyMap[operands[0]].dimensions
+        };
+    }
     return success();
 }
 
--- lib/Analysis/AlgebraicStructureAnalysis.h
+++ lib/Analysis/AlgebraicStructureAnalysis.h
@@ -4,21 +4,11 @@
 #include "mlir/IR/Value.h"
 #include "mlir/IR/Operation.h"
 #include "mlir/Support/LLVM.h"
-#include <string>
-#include <iostream>
-#include <limits>
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include <array>
+#include "lib/Analysis/AlgebraicProperty.h"
 
 namespace mlir::asa {
-
-enum class AlgebraicProperty : unsigned int {
-    Identity,
-    Diagonal,
-    Symmetric,
-    UpperTriangular,
-    LowerTriangular,
-    General,
-};
 
 struct SubMatrixProperty {
     AlgebraicProperty property;
@@ -31,114 +21,6 @@ struct SubMatrixProperty {
     }
 };
 
-static AlgebraicProperty meet(const AlgebraicProperty lhs, const AlgebraicProperty rhs) {
-    if (lhs == AlgebraicProperty::Identity)
-        return rhs;
-    if (rhs == AlgebraicProperty::Identity)
-        return lhs;
-    if (lhs == AlgebraicProperty::Diagonal)
-        return rhs;
-    if (rhs == AlgebraicProperty::Diagonal)
-        return lhs;
-    if (lhs == rhs)
-        return lhs;
-    return AlgebraicProperty::General;
-}
-
-static AlgebraicProperty join(const AlgebraicProperty& lhs, const AlgebraicProperty& rhs) {
-    // pessimistic join
-    if (lhs == AlgebraicProperty::General)
-        return rhs;
-    if (rhs == AlgebraicProperty::General)
-        return lhs;
-    if (lhs == AlgebraicProperty::Symmetric || lhs == AlgebraicProperty::UpperTriangular || lhs == AlgebraicProperty::LowerTriangular)
-        return AlgebraicProperty::Diagonal; // rhs is either sym, upper, lower, or diagonal. either way, output is diagonal
-    if (rhs == AlgebraicProperty::Symmetric || rhs == AlgebraicProperty::UpperTriangular || rhs == AlgebraicProperty::LowerTriangular)
-        return AlgebraicProperty::Diagonal; // rhs is either sym, upper, lower, or diagonal. either way, output is diagonal
-    if (lhs == AlgebraicProperty::Diagonal)
-        return rhs;
-    if (rhs == AlgebraicProperty::Diagonal)
-        return lhs;
-    if (lhs == AlgebraicProperty::Identity)
-        return rhs;
-    if (rhs == AlgebraicProperty::Identity)
-        return lhs;
-    return lhs; // identity
-}
-
-static std::string propertyToString(AlgebraicProperty property) {
-    switch (property) {
-        case AlgebraicProperty::Identity:
-            return "Identity";
-        case AlgebraicProperty::Diagonal:
-            return "Diagonal";
-        case AlgebraicProperty::Symmetric:
-            return "Symmetric";
-        case AlgebraicProperty::UpperTriangular:
-            return "UpperTriangular";
-        case AlgebraicProperty::LowerTriangular:
-            return "LowerTriangular";
-        case AlgebraicProperty::General:
-            return "General";
-    }
-    return "General";
-}
-
-static const AlgebraicProperty stringToValue(const std::string& value) {
-    static const DenseMap<StringRef, AlgebraicProperty> dict {
-        { "Identity", AlgebraicProperty::Identity },
-        { "Diagonal", AlgebraicProperty::Diagonal },
-        { "Symmetric", AlgebraicProperty::Symmetric },
-        { "UpperTriangular", AlgebraicProperty::UpperTriangular },
-        { "LowerTriangular", AlgebraicProperty::LowerTriangular },
-        { "General", AlgebraicProperty::General },
-    };
-    return dict.contains(value) ? dict.at(value) : AlgebraicProperty::General;
-}
-
-static AlgebraicProperty binaryMatmul(AlgebraicProperty lhs, AlgebraicProperty rhs) {
-    if (lhs == AlgebraicProperty::Identity && lhs == rhs)
-        return AlgebraicProperty::Identity;
-    if (lhs == AlgebraicProperty::Diagonal && lhs == rhs)
-        return AlgebraicProperty::Diagonal;
-    if (lhs == AlgebraicProperty::LowerTriangular && lhs == rhs)
-        return AlgebraicProperty::LowerTriangular;
-    if (lhs == AlgebraicProperty::UpperTriangular && lhs == rhs)
-        return AlgebraicProperty::UpperTriangular;
-    return AlgebraicProperty::General;
-}
-
-static AlgebraicProperty binaryElementwiseGeneral(AlgebraicProperty lhs, AlgebraicProperty rhs) {
-    if (lhs == AlgebraicProperty::Identity && lhs == rhs)
-        return AlgebraicProperty::Diagonal;
-    if (lhs == AlgebraicProperty::Diagonal && lhs == rhs)
-        return AlgebraicProperty::Diagonal;
-    if (lhs == AlgebraicProperty::Symmetric && lhs == rhs)
-        return AlgebraicProperty::Symmetric;
-    if (lhs == AlgebraicProperty::LowerTriangular && lhs == rhs)
-        return AlgebraicProperty::LowerTriangular;
-    if (lhs == AlgebraicProperty::UpperTriangular && lhs == rhs)
-        return AlgebraicProperty::UpperTriangular;
-    return AlgebraicProperty::General;
-}
-
-static AlgebraicProperty binaryElementwiseProduct(AlgebraicProperty lhs, AlgebraicProperty rhs) {
-    return join(lhs, rhs);
-}
-
-static AlgebraicProperty unaryElementwise(AlgebraicProperty property) {
-    if (property == AlgebraicProperty::Identity)
-        return AlgebraicProperty::Diagonal;
-    return property;
-}
-
-static AlgebraicProperty transpose(AlgebraicProperty property) {
-    if (property == AlgebraicProperty::UpperTriangular)
-        return AlgebraicProperty::LowerTriangular;
-    if (property == AlgebraicProperty::LowerTriangular)
-        return AlgebraicProperty::UpperTriangular;
-    return property;
-}
 
 class AlgebraicStructureAnalysis {
     llvm::DenseMap<mlir::Value, SubMatrixProperty> propertyMap;
@@ -154,31 +36,12 @@ public:
 
 private:
     LogicalResult visitOperation(Operation* op);
-    SubMatrixProperty readPropertyFromDictAttr(DictionaryAttr dictAttr) {
-        auto metadataAttr{ dictAttr.get("metadata") };
-        if (!metadataAttr)
-            return { AlgebraicProperty::General };
-        auto innerDictAttr{ dyn_cast<DictionaryAttr>(metadataAttr) };
-        if (!innerDictAttr)
-            return { AlgebraicProperty::General };
-        auto analysisPropertyAttr{ innerDictAttr.get("analysisState") };
-        if (!analysisPropertyAttr)
-            return { AlgebraicProperty::General };
-        auto analysisProperty{ dyn_cast<StringAttr>(analysisPropertyAttr).getValue() };
-
-        auto propertyDimsAttr{ innerDictAttr.get("propertyDims") };
-        if (!propertyDimsAttr)
-            return { AlgebraicProperty::General };
-        auto propertyDimsArrayAttr{ dyn_cast<ArrayAttr>(propertyDimsAttr) };
-        if (!propertyDimsArrayAttr || propertyDimsArrayAttr.size() != 2) {
-            return { AlgebraicProperty::General };
-        }
-        SubMatrixProperty res{ stringToValue(std::string(analysisProperty)) }; 
-        res.dimensions[0] = cast<IntegerAttr>(propertyDimsArrayAttr[0]).getInt();
-        res.dimensions[1] = cast<IntegerAttr>(propertyDimsArrayAttr[1]).getInt();
-
-        return res;
-    }
+    LogicalResult visitMatmul(linalg::MatmulOp* );
+    LogicalResult visitAdd(linalg::AddOp* addOp);
+    LogicalResult visitElementwise(linalg::ElementwiseOp* elementwiseOp);
+    LogicalResult visitMul(linalg::MulOp* mulOp);
+    LogicalResult visitTranspose(linalg::TransposeOp* transposeOp);
+    SubMatrixProperty readPropertyFromDictAttr(DictionaryAttr dictAttr);
 };
 }
 
