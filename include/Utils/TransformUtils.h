cd12d99d503858eaea3daff65aa5dd801a4ace18
Kaio Henrique <deandradekaio@gmail.com>
2026-03-04 17:28:28 -0700

    fix: only propagates dia layouts for output tensors

--- include/Utils/TransformUtils.h
+++ include/Utils/TransformUtils.h
@@ -1,12 +1,25 @@
 #include "Analysis/BandedStructureAnalysis.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/Operation.h"
-#include "mlir/IR/PatternMatch.h"
 
 namespace mlir::bpa {
 
-inline bool shouldCompress(const BandedSubMatrix& resultBand, const uint64_t N) {
-    return (resultBand.Property.LowerBandwidth + resultBand.Property.UpperBandwidth) < N;
+// Write result to DIA format only when:
+// The result is already set to DIA
+// Flag `detect-dia=true`
+// Instruction is supported by the analysis (which means that only forward propagation change
+// layouts
+// Compress overhead is smaller than dense space waste
+inline bool shouldCompressResult(const bool detectDia, Operation& op,
+                                 const BandedSubMatrix& resultBand, const uint64_t N) {
+    const bool isAlreadyDIA = resultBand.IsDia;
+    auto dialect = op.getDialect();
+    const bool isDialectSupported =
+        (dialect && (dialect->getNamespace() == "linalg" || dialect->getNamespace() == "dia"));
+    const bool isOverheadSmaller =
+        (resultBand.Property.LowerBandwidth + resultBand.Property.UpperBandwidth) < N;
+
+    return isAlreadyDIA || (detectDia && isDialectSupported && isOverheadSmaller);
 }
 
 }  // namespace mlir::bpa
--- tests/Transform/test-dia-to-dia-diag-mm-rewrite-to-linalg.mlir
+++ tests/Transform/test-dia-to-dia-diag-mm-rewrite-to-linalg.mlir
@@ -5,11 +5,13 @@
 // CHECK-LABEL: func.func @main
 // CHECK-SAME: -> tensor<1x4xf32>
 module {
+  func.func private @printMemrefF32(memref<*xf32>)
+
   func.func @main() -> tensor<1x4xf32> {
     %dia1 = arith.constant {metadata = {dia = true, upperBw = 0 : i64, lowerBw = 0 : i64, propertyDims = [0, 1]}}
-        dense<[[0.0, 1.0, 2.0, 3.0]]> : tensor<1x4xf32>
+        dense<[[1.0, 2.0, 3.0, 4.0]]> : tensor<1x4xf32>
     %dia2 = arith.constant {metadata = {dia = true, upperBw = 0 : i64, lowerBw = 0 : i64, propertyDims = [0, 1]}}
-        dense<[[0.0, 1.0, 2.0, 3.0]]> : tensor<1x4xf32>
+        dense<[[1.0, 2.0, 3.0, 4.0]]> : tensor<1x4xf32>
     %0 = tensor.empty() : tensor<1x4xf32>
     %1 = dia.matmul ins(%dia1, %dia2 : tensor<1x4xf32>, tensor<1x4xf32>)
                     outs(%0 : tensor<1x4xf32>) -> tensor<1x4xf32>
@@ -21,6 +23,7 @@ module {
     // CHECK: arith.mulf
     // CHECK: linalg.yield
     // CHECK-NOT: dia.matmul
+    //
 
     return %1 : tensor<1x4xf32>
   }
