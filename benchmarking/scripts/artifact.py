c024fa08ed137f4d5f106a9d7d286cc9898a75be
Kaio Henrique <deandradekaio@gmail.com>
2026-04-11 19:42:54 -0600

    feat: batch bertlike graph

--- benchmarking/scripts/artifact.py
+++ benchmarking/scripts/artifact.py
@@ -48,6 +48,29 @@ def kalman_filter():
     figures.bandwidth_plot(result_path, benchmark_name, "Kalman Filter Benchmark")
 
 
+def batch_bertlike():
+    benchmark_name = "batch_bertlike"
+    bandwidths = [512, 409, 307, 256, 204, 153, 102, 51, 0]
+
+    configs = [
+        ("batch_bertlike_dense.mlir", None),
+        ("batch_bertlike_dense.mlir", "analysis", "rewrite"),
+        ("batch_bertlike_dia.mlir", "analysis", "rewrite"),
+        ("batch_bertlike_dia.mlir", "analysis-detect", "rewrite"),
+    ]
+
+    result_path = bench.run(
+        benchmark_name=benchmark_name,
+        program_dir="./benchmarking/programs",
+        configs=configs,
+        bandwidths=bandwidths,
+        warmup=1,
+        runs_count=5,
+    )
+    figures.bandwidth_plot(result_path, benchmark_name, "Batch Bert-Like Benchmark")
+
+
 if __name__ == "__main__":
     chain_matmul()
     kalman_filter()
+    batch_bertlike()
