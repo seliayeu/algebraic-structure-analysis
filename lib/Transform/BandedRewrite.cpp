7428f0f89ac9730e5a8d1427d5cef829dfb80eb2
Danila Seliayeu <seliayeu@ualberta.ca>
2026-04-15 09:19:59 -0600

    fix: improve linalg matmul lowering loop order

--- lib/Transform/BandedRewrite.cpp
+++ lib/Transform/BandedRewrite.cpp
@@ -274,76 +274,55 @@ struct MatMulPattern : public OpRewritePattern<linalg::MatmulOp> {
         Value lowerB = arith::ConstantIndexOp::create(rewriter, loc, bandB.Property.LowerBandwidth);
         Value upperB = arith::ConstantIndexOp::create(rewriter, loc, bandB.Property.UpperBandwidth);
 
-        // WARNING: CI was failing because of unitialized data. This fix it. Does it hurst dense
-        // performance?
         auto elementType = resultType.getElementType();
         Value zero = arith::ConstantOp::create(rewriter, loc, elementType,
                                                rewriter.getZeroAttr(elementType));
         Value zeroedC =
             linalg::FillOp::create(rewriter, loc, ValueRange{ zero }, ValueRange{ C }).getResult(0);
 
-        // C is the projection of the Minkowski sum of A and B bands.
-        //----------------------------------------------------------
-        // for i in [0, N)
-        //  for k in  [max(0, i-lower), min(M, i+upper)]
-        //    for j in [max(i - La, k - Ub), min(i + Ua, k + Lb)]
-        //        C[i,k] += A[i,j] * B[j,k]
         auto iLoop = scf::ForOp::create(
             rewriter, loc, c0, dimN, c1, ValueRange{ zeroedC },
             [&](OpBuilder& ob, Location loc, Value i, ValueRange iArgs) {
-                // C
                 Value cOut = iArgs[0];
-                // k start = max(0, i - (La + Lb))
-                Value lAPlusLb = arith::AddIOp::create(ob, loc, lowerA, lowerB);
-                Value iMinusLower = arith::SubIOp::create(ob, loc, i, lAPlusLb);
-                Value kStart = arith::MaxSIOp::create(ob, loc, c0, iMinusLower);
 
-                // k end = min(M, i + (Ua + Ub))
-                Value uAPlusUb = arith::AddIOp::create(ob, loc, upperA, upperB);
-                Value iPlusUpper = arith::AddIOp::create(ob, loc, i, uAPlusUb);
-                Value iPlusUpperP1 = arith::AddIOp::create(ob, loc, iPlusUpper, c1);
-                Value kEnd = arith::MinSIOp::create(ob, loc, dimM, iPlusUpperP1);
-                // k loop
-                auto kLoop = scf::ForOp::create(
-                    ob, loc, kStart, kEnd, c1, ValueRange{ cOut },
-                    [&](OpBuilder& mb, Location loc, Value k, ValueRange kArgs) {
-                        auto cMid = kArgs[0];
-                        // j start = max(i-La, k - Ub)
-                        Value iMinusLa = arith::SubIOp::create(mb, loc, i, lowerA);
-                        Value kMinusUb = arith::SubIOp::create(mb, loc, k, upperB);
-                        Value jStart = arith::MaxSIOp::create(
-                            mb, loc, arith::MaxSIOp::create(mb, loc, kMinusUb, iMinusLa), c0);
+                Value iMinusLa = arith::SubIOp::create(ob, loc, i, lowerA);
+                Value jStart = arith::MaxSIOp::create(ob, loc, c0, iMinusLa);
 
-                        // j end = min(i + Ua, k + Lb)
-                        Value iPlusUa = arith::AddIOp::create(mb, loc, i, upperA);
-                        Value kPlusLb = arith::AddIOp::create(mb, loc, k, lowerB);
-                        Value jEndPlusOne = arith::AddIOp::create(
-                            mb, loc, arith::MinSIOp::create(mb, loc, kPlusLb, iPlusUa), c1);
-                        Value jEnd = arith::MinSIOp::create(
-                            mb, loc, jEndPlusOne, arith::ConstantIndexOp::create(mb, loc, N));
-                        auto jLoop = scf::ForOp::create(
-                            mb, loc, jStart, jEnd, c1, ValueRange{ cMid },
-                            [&](OpBuilder& ib, Location loc, Value j, ValueRange jArgs) {
-                                Value cInner = jArgs[0];
-                                // Load current C[i,k]
+                Value iPlusUa = arith::AddIOp::create(ob, loc, i, upperA);
+                Value iPlusUaP1 = arith::AddIOp::create(ob, loc, iPlusUa, c1);
+                Value jEnd = arith::MinSIOp::create(ob, loc, dimN, iPlusUaP1);
+
+                auto jLoop = scf::ForOp::create(
+                    ob, loc, jStart, jEnd, c1, ValueRange{ cOut },
+                    [&](OpBuilder& mb, Location loc, Value j, ValueRange jArgs) {
+                        Value cMid = jArgs[0];
+
+                        Value aij = tensor::ExtractOp::create(mb, loc, A, ValueRange{ i, j });
+
+                        Value jMinusUb = arith::SubIOp::create(mb, loc, j, upperB);
+                        Value kStart = arith::MaxSIOp::create(mb, loc, c0, jMinusUb);
+
+                        Value jPlusLb = arith::AddIOp::create(mb, loc, j, lowerB);
+                        Value jPlusLbP1 = arith::AddIOp::create(mb, loc, jPlusLb, c1);
+                        Value kEnd = arith::MinSIOp::create(mb, loc, dimM, jPlusLbP1);
+
+                        auto kLoop = scf::ForOp::create(
+                            mb, loc, kStart, kEnd, c1, ValueRange{ cMid },
+                            [&](OpBuilder& ib, Location loc, Value k, ValueRange kArgs) {
+                                Value cInner = kArgs[0];
                                 Value cik =
                                     tensor::ExtractOp::create(ib, loc, cInner, ValueRange{ i, k });
-                                // Load A[i,j] and B[j,k]
-                                Value aij =
-                                    tensor::ExtractOp::create(ib, loc, A, ValueRange{ i, j });
                                 Value bjk =
                                     tensor::ExtractOp::create(ib, loc, B, ValueRange{ j, k });
-                                // C[i,k] += A[i,j] * B[j,k]
                                 Value mul = arith::MulFOp::create(ib, loc, aij, bjk);
                                 Value add = arith::AddFOp::create(ib, loc, cik, mul);
-                                // Insert updated value back
                                 Value updated = tensor::InsertOp::create(ib, loc, add, cInner,
                                                                          ValueRange{ i, k });
                                 scf::YieldOp::create(ib, loc, ValueRange{ updated });
                             });
-                        scf::YieldOp::create(mb, loc, jLoop.getResults());
+                        scf::YieldOp::create(mb, loc, kLoop.getResults());
                     });
-                scf::YieldOp::create(ob, loc, kLoop.getResults());
+                scf::YieldOp::create(ob, loc, jLoop.getResults());
             });
         iLoop->setAttr("metadata", op->getAttr("metadata"));
         rewriter.replaceOp(op, iLoop.getResult(0));
