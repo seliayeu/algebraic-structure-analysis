1cfd34cce4ea323923776edaa726193e05d98fef
Kaio Henrique <deandradekaio@gmail.com>
2026-04-27 17:11:53 -0600

    chore: cleaning bench.py

--- benchmarking/scripts/bench.py
+++ benchmarking/scripts/bench.py
@@ -5,38 +5,7 @@ import subprocess
 import csv
 from typing import List, Tuple
 import logging
-
-
-class CustomFormatter(logging.Formatter):
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    green = "\x1b[32;20m"
-    cyan = "\x1b[36;20m"
-    blue = "\x1b[34;20m"
-    magenta = "\x1b[35;20m"
-    bold = "\x1b[1m"
-    reset = "\x1b[0m"
-
-    def format(self, record):
-        if record.levelno == logging.INFO:
-            self._style._fmt = f"{self.green}[%(asctime)s]{self.reset} {self.blue}%(message)s{self.reset}"
-        elif record.levelno == logging.WARNING:
-            self._style._fmt = (
-                f"{self.yellow}[%(asctime)s] WARNING: %(message)s{self.reset}"
-            )
-        elif record.levelno == logging.ERROR:
-            self._style._fmt = f"{self.red}[%(asctime)s] ERROR: %(message)s{self.reset}"
-        elif record.levelno == logging.DEBUG:
-            self._style._fmt = (
-                f"{self.grey}[%(asctime)s] DEBUG: %(message)s{self.reset}"
-            )
-        else:
-            self._style._fmt = f"[%(asctime)s] %(message)s"
-
-        self._style._fmt = f"{self.bold}[%(levelname)s]{self.reset} " + self._style._fmt
-        return super().format(record)
-
+from custom_logging import CustomFormatter
 
 logging.basicConfig(
     level=logging.INFO, format="[%(asctime)s] %(message)s", datefmt="%H:%M:%S"
@@ -47,7 +16,7 @@ for handler in logger.handlers:
     handler.setFormatter(CustomFormatter())
 
 BUILD_DIR = Path("./build")
-TMP_DIR = Path("/tmp/mlir_bench")
+TMP_DIR = Path("/tmp/bpa_main")
 RESULT_DIR = Path("./results")
 
 
