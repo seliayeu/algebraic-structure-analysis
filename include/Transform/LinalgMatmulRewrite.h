f9967a082b6db97c8b70bf2e03e56ffde8c4029e
Kaio Henrique <deandradekaio@gmail.com>
2026-05-19 12:02:33 -0600

    feat: optimize batch bertlike with ikj ordering

--- include/Transform/LinalgMatmulRewrite.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIB_TRANSFORMS_LINALGMATMULREWRITE_H
-#define LIB_TRANSFORMS_LINALGMATMULREWRITE_H
-#include "mlir/Pass/Pass.h"
-
-namespace mlir::bpa {
-#define GEN_PASS_DECL_LINALGMATMULREWRITE
-#include "lib/Transform/Passes.h.inc"
-}  // namespace mlir::bpa
-#endif
